3c7de18e65e88cb7a900ff7e1ad11c0d178c6f1a
Luke Hurst <hurstlj@umich.edu>
2026-07-28 00:07:35 -0700

    Use OIDC for deploy (#2)

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -2,3 +2,8 @@ output "s3_website_url" {
   description = "S3 website URL"
   value       = "http://${aws_s3_bucket_website_configuration.static_site.website_endpoint}"
 }
+
+output "github_actions_frontend_deploy_role_arn" {
+  description = "IAM role ARN for GitHub Actions frontend deploy"
+  value       = aws_iam_role.github_actions_frontend_deploy.arn
+}
